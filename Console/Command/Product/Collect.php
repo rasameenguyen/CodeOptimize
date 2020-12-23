fa9d10f676df7e69a99a9dd22814f3311cf515df
Nguyen Thi Hang <hangnt@magenest.com>
2020-12-23 16:20:38 +0700

    5sao-202: example about the count() function with for loop in php

---
+++ Console/Command/Product/Collect.php
@@ -0,0 +1,128 @@
+<?php
+namespace Magenest\CodeOptimize\Console\Command\Product;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Magento\Framework\App\State as StateAppFramework;
+use Magento\Catalog\Api\ProductRepositoryInterface;
+use Magento\Catalog\Model\ResourceModel\Product\CollectionFactory as ProductCollectionFactory;
+
+/**
+ * Class Collect
+ * @package Magenest\CodeOptimize\Console\Command\Product
+ */
+class Collect extends Command
+{
+    const CLI_NAME = 'time:issue';
+    const ISSUE_NUMBER = 'issue_number';
+    const ISSUE_NUMBER_1 = 1;
+    const ISSUE_NUMBER_2 = 2;
+    const ISSUE_NUMBER_3 = 3;
+    const ISSUE_NUMBER_4 = 4;
+    const ISSUE_NUMBER_5 = 5;
+
+
+    /** @var StateAppFramework  */
+    protected $appState;
+
+    /** @var ProductRepositoryInterface  */
+    protected $productRepository;
+
+    /** @var ProductCollectionFactory  */
+    protected $productCollectionFactory;
+
+    public function __construct(
+        StateAppFramework $appState,
+        ProductRepositoryInterface $productRepository,
+        ProductCollectionFactory $productCollectionFactory,
+        string $name = null
+    ) {
+        $this->appState = $appState;
+        $this->productRepository = $productRepository;
+        $this->productCollectionFactory = $productCollectionFactory;
+        parent::__construct($name);
+    }
+
+    protected function configure()
+    {
+        $options = [
+            new InputOption(
+                self::ISSUE_NUMBER_1,
+                null,
+                InputOption::VALUE_NONE,
+                'run issue number 1'
+            ),
+            new InputOption(
+                self::ISSUE_NUMBER_2,
+                null,
+                InputOption::VALUE_NONE,
+                'run issue number 2'
+            ),
+            new InputOption(
+                self::ISSUE_NUMBER_3,
+                null,
+                InputOption::VALUE_NONE,
+                'run issue number 3'
+            ),
+            new InputOption(
+                self::ISSUE_NUMBER_4,
+                null,
+                InputOption::VALUE_NONE,
+                'run issue number 4'
+            ),
+            new InputOption(
+                self::ISSUE_NUMBER_5,
+                null,
+                InputOption::VALUE_NONE,
+                'run issue number 5'
+            )
+        ];
+
+        $this->setName(self::CLI_NAME)->setDescription('Run issue with number')->setDefinition($options);
+        parent::configure();;
+    }
+
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @return int|void
+     */
+    public function execute(InputInterface $input, OutputInterface $output)
+    {
+        try {
+            $this->appState->setAreaCode(\Magento\Framework\App\Area::AREA_GLOBAL);
+            $output->writeln("Begin: ...");
+            if($input->getOption(self::ISSUE_NUMBER_1) ){
+               $this->runIssues1($output);
+            }
+            $output->writeln("Completed!");
+        } catch (\Exception $exception){
+            $output->writeln($exception->getMessage());
+        }
+    }
+
+    /**
+     * @param $output
+     * @throws \Exception
+     */
+    private function runIssues1($output)
+    {
+        try {
+            $startTime = microtime(true);
+            $productCollection = $this->productCollectionFactory->create();
+            $productCollection->load();
+            $count = 0;
+//            $a = count($productCollection);
+            for ($i = 0; $i < count($productCollection); $i++) {
+                $count++;
+            }
+            $endTime = microtime(true);
+            $time = $endTime - $startTime;
+            $output->writeln("Execute time: ".$time);
+        } catch (\Exception $exception) {
+            throw new \Exception($exception->getMessage());
+        }
+    }
+}
